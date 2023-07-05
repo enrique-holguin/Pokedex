92adb10ee94be04435942c62728fc4b2512dbeef
Enrique <e.holguin993@gmail.com>
2023-07-04 20:10:09 -0500

    gitignore mongo files y schema Pokemon

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -9,8 +10,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
       rootPath: join(__dirname, '../public'),
     }),
     PokemonModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
